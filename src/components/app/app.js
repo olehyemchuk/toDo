00dba683b92dfe3840fb4f46615f9f05403dd4b7
Oleh <emchuk86@gmail.com>
2019-09-30 17:36:09 +0300

    toDo

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
-
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
 import ItemAddForm from "../item-add-form";
-
 import './app.css';
 
 export default class App extends Component {
