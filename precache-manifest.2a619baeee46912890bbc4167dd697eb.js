e5f088c7c53af5adf12337f7924ef5e39ad84655
Oleh <emchuk86@gmail.com>
2020-03-27 12:47:52 +0200

    Updates

---
+++ precache-manifest.2a619baeee46912890bbc4167dd697eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11eef7a0923372c2e0a189ba7e90b865",
+    "url": "/Yemchuk/toDo.git/index.html"
+  },
+  {
+    "revision": "d954fe0122bec85953ba",
+    "url": "/Yemchuk/toDo.git/static/css/main.da59d705.chunk.css"
+  },
+  {
+    "revision": "53bf6351f5b5ee97a9bd",
+    "url": "/Yemchuk/toDo.git/static/js/2.8ba249f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Yemchuk/toDo.git/static/js/2.8ba249f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d954fe0122bec85953ba",
+    "url": "/Yemchuk/toDo.git/static/js/main.8e316373.chunk.js"
+  },
+  {
+    "revision": "8990f90a86c74d07fd5d",
+    "url": "/Yemchuk/toDo.git/static/js/runtime-main.33928291.js"
+  }
+]);
